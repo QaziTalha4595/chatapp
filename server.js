48d19af61bd110f414ece1f47ac256e13edf3325
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2023-06-27 11:34:29 +0530

    errt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const app = express();
+const fs = require('fs');
 const server = http.createServer(app);
 app.use(express.static(__dirname + '/public'));
 const io = socketIO(server);
@@ -26,10 +27,17 @@ io.on('connection', (socket) => {
 
   // Handle new message event
   socket.on('message', (data) => {
-    io.emit('message', {
+    const message = {
       username: data.username,
-      message: data.message
-    });
+      message: data.message,
+      timestamp: new Date().getTime()
+    };
+
+    // Save the message to messages.json
+    saveMessage(message);
+
+    io.emit('message', message);
+
   });
 
   // Handle disconnect event
@@ -43,7 +51,37 @@ io.on('connection', (socket) => {
 });
 
 
-const port = 3000; // Specify the desired port number
+function saveMessage(message) {
+  const filePath = __dirname + '/public/messages.json';
+
+  // Read existing messages from the file
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading messages.json:', err);
+      return;
+    }
+
+    let messages = [];
+    try {
+      messages = JSON.parse(data);
+    } catch (error) {
+      console.error('Error parsing messages.json:', error);
+      return;
+    }
+
+    // Add the new message to the array
+    messages.push(message);
+
+    // Write the updated messages back to the file
+    fs.writeFile(filePath, JSON.stringify(messages), 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing messages.json:', err);
+      }
+    });
+  });
+}
+
+const port = 4595; // Specify the desired port number
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
